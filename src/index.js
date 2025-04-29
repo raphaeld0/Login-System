48341554a441a370878a5b22050b45c07519648c
Raphael dias <rd78dias@gmail.com>
2025-04-28 23:10:29 -0300

    git

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ require("dotenv").config(); //config do dotenv
 
 require("../src/modules/express"); // require do express
 
+require("../src/modules/render")
+
 const connectToDatabase = require("../src/modules/connect"); //require pro export do database(connect.js)
 connectToDatabase();// conectando ao database
